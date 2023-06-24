4444d08ec6485f6f69fd1e7cde206911ef287ced
labsxbit <115821738+labsxbit@users.noreply.github.com>
2023-06-24 15:18:41 +0530

    Add files via upload

---
+++ java/RECO839YPU10032023Ri/certificate.js
@@ -0,0 +1,17 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Ritika Kushwaha";
+
+// Set the program name
+document.getElementById("programName").textContent = "Java Programming ( Integrated Level 1 and Level 2 ) ";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on March 10, 2023";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Ritika Kushwaha has demonstrated high level of proficiency and understanding of the course material. We commend her for the dedication and hard work during the course.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://training.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
