8e1644be42de47ad3ddc19f8168ea9cc0c06ef67
labsxbit <labsxbit@gmail.com>
2023-06-26 15:58:05 +0530

    6ZKHZMYCO414042022Am

---
+++ java/sample-certificate/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Ruchicha Bharti";
+
+// Set the program name
+document.getElementById("programName").textContent = "Java Programming (Level 1) ";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on March 10, 2023";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : Y9MWKUW66V10032023Ru";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Ruchicha Bharti has demonstrated high level of proficiency and understanding of the course material. We commend her for the dedication and hard work during the course.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://training.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
---
+++ python/6ZKHZMYCO414042022Am/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Amosh Kumar Rohit";
+
+// Set the program name
+document.getElementById("programName").textContent = "Python Programming ( Integrated Level 1 & Level 2 ) ";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on April 14, 2022";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : 6ZKHZMYCO414042022Am";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Amosh Kumar Rohit has demonstrated high level of proficiency and understanding of the course material. We commend him for the dedication and hard work during the course.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://training.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
