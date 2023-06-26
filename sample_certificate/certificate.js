52091411f5c37dbe03bf013484af8f6222578223
labsxbit <115821738+labsxbit@users.noreply.github.com>
2023-06-26 12:16:24 +0530

    Update certificate.js

--- sample_certificate/certificate.js
+++ sample_certificate/certificate.js
@@ -7,6 +7,9 @@ document.getElementById("programName").textContent = "Software Development Train
 // Set the completion date
 document.getElementById("completionDate").textContent = "Completed on June 26, 2023";
 
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : 9TIVW9UDOE01112000Jo";
+
 // Set the achievement text
 document.getElementById("achievementText").textContent = "John Doe has demonstrated exceptional knowledge and skills in the field of software development during their engagement with XBit Labs. They have shown dedication, proficiency, and a commitment to continuous learning.";
 
