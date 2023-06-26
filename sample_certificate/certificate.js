b6f5a0b2ab066359d1b8e2748d65992887f317e0
labsxbit <115821738+labsxbit@users.noreply.github.com>
2023-06-26 11:59:09 +0530

    Add files via upload

---
+++ sample_certificate/certificate.js
@@ -0,0 +1,17 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "john Doe";
+
+// Set the program name
+document.getElementById("programName").textContent = "Software Development Training";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on June 26, 2023";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "John Doe has demonstrated exceptional knowledge and skills in the field of software development during their engagement with XBit Labs. They have shown dedication, proficiency, and a commitment to continuous learning.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://training.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
