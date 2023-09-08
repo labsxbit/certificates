39b1ccbf6cd38cb0c13b35e798fe15a665f15723
labsxbit <115821738+labsxbit@users.noreply.github.com>
2023-09-08 07:02:31 -0400

    Add files via upload

---
+++ internship/digital-marketing/YMK9T56V1Z04082023Ay/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Ayush Sharma";
+
+// Set the program name
+document.getElementById("programName").textContent = "Digital Marketing Training & Internship";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on August 04, 2023";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : "YMK9T56V1Z04082023Ay";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Ayush Sharma has successfully completed a one-month digital marketing internship at XBit Labs, and his dedication was commendable.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
