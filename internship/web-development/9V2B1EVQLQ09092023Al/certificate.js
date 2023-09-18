33c9818dcb35cbc90aa5c439e1e0817acbd5729c
labsxbit <labsxbit@gmail.com>
2023-09-18 23:01:43 +0530

    added new certificate

---
+++ internship/web-development/9V2B1EVQLQ09092023Al/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Alyssa Eilermann";
+
+// Set the program name
+document.getElementById("programName").textContent = "Website Development Engineer ( Internship )";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on September 09, 2023";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : 9V2B1EVQLQ09092023Al";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Alyssa Eilermann excelled during her Website Development Engineer internship at XBit Labs, demonstrating a keen aptitude for web development, a commitment to excellence, and a deep understanding of modern web technologies.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
