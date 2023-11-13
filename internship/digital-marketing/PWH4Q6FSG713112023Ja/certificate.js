e39c9ac50fb6e1ba23dbca48e0664b94205591d3
labsxbit <labsxbit@gmail.com>
2023-11-13 20:37:43 +0530

    added certificate for javeria

---
+++ internship/digital-marketing/PWH4Q6FSG713112023Ja/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Javeria";
+
+// Set the program name
+document.getElementById("programName").textContent = "Digital Marketing Training & Internship";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on November 13, 2023";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : PWH4Q6FSG713112023Ja";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Javeria's outstanding contributions have been instrumental, excelling in all facets of the project requirements throughout her 6-7 months of internship. Her dedication and stellar performance have been invaluable, and we look forward to maintaining this high standard of excellence in our continued collaboration on future projects.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
