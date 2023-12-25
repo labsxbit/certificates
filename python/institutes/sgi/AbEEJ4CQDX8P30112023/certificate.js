c087d3d83da1f1a4abbeece8d01445f8c3cbf4ff
labsxbit <labsxbit@gmail.com>
2023-12-25 21:27:37 +0530

    testing

---
+++ python/institutes/sgi/AbEEJ4CQDX8P30112023/certificate.js
@@ -0,0 +1,20 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "john Doe";
+
+// Set the program name
+document.getElementById("programName").textContent = "Software Development Training";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on June 26, 2023";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : 9TIVW9UDOE01112000Jo";
+
+// Set the achievement text
+document.getElementById("achievementText").textContent = "John Doe has demonstrated exceptional knowledge and skills in the field of software development during their engagement with XBit Labs. They have shown dedication, proficiency, and a commitment to continuous learning.";
+
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "http://training.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs";
