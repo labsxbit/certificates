94406d8750287714026c43be15092af9ec2b6dc3
labsxbit <sagar.xbitlabs@gmail.com>
2024-08-07 12:59:51 +0530

    updated

---
+++ Experience-Letters/Q1JQB55ZQM15062024Po/certificate.js
@@ -0,0 +1,18 @@
+// Set the recipient's name
+document.getElementById("recipientName").textContent = "Pooja Sorty";
+
+// Set the program name
+document.getElementById("programName").textContent = "Data Research Analyst";
+
+// Set the completion date
+document.getElementById("completionDate").textContent = "Completed on June 15, 2024";
+
+// set the certificationId
+document.getElementById("certificationId").textContent = "Certification ID : Q1JQB55ZQM15062024Po";
+// Set the achievement text
+document.getElementById("achievementText").textContent = "Pooja Sorty has completed a training and internship program at XBit Labs IN Pooja worked as a Data Research Analyst from March 2024 to June 2024. She did a great job gathering and analyzing data for our projects. Pooja's careful attention to detail and her skills with data tools helped us understand our data better and make good decisions. We really liked her work and appreciated her hard work and quality results during this time."
+// Set the signatory name
+document.getElementById("signatoryName").textContent = "https://www.xbitlabs.in";
+
+// Set the signatory title
+document.getElementById("signatoryTitle").textContent = "XBit Labs IN";
